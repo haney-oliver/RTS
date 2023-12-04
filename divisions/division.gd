06591a730db466027d7def429a09a29de5f493c6
haney-oliver <haney.oliver@gmail.com>
2023-12-04 10:36:36 -0500

    Add terrain speed modifiers

--- divisions/division.gd
+++ divisions/division.gd
@@ -14,10 +14,10 @@ const WALK_BLEND_POS : String = "parameters/Walk/blend_position"
 
 var current_path: Array[Vector2i]
 
-func _ready():
+func _ready() -> void:
 	update_animation_params(starting_direction)
 	
-func _input(_event):
+func _input(_event) -> void:
 	if _event.is_action_pressed("select"):
 		var from: Vector2i = tile_map.local_to_map(self.global_position)
 		var mouse_pos: Vector2 = self.get_global_mouse_position()
@@ -27,24 +27,37 @@ func _input(_event):
 			if !path.is_empty():
 				current_path = path
 		
-func update_animation_params(_move_input : Vector2):
+func update_animation_params(_move_input : Vector2) -> void:
 	if _move_input != Vector2.ZERO:
 		animation_tree.set(WALK_BLEND_POS, _move_input)
 		animation_tree.set(IDLE_BLEND_POS, _move_input)
 		
-func pick_new_state():
+func pick_new_state() -> void:
 	if self.velocity != Vector2.ZERO:
 		state_machine.travel("Walk")
 	else:
 		state_machine.travel("Idle")
 		
-func _physics_process(_delta):
+func _physics_process(_delta) -> void:
 	if !current_path.is_empty():
 		var current_pos: Vector2 = self.global_position
+		var current_map_pos: Vector2i = tile_map.local_to_map(current_pos)
+		var tile_data: TileData = tile_map.get_cell_tile_data(1, current_map_pos)
+		var speed_modifier: float = get_terrain_speed_modifier(tile_data)
 		var target_pos: Vector2 = tile_map.map_to_local(current_path.front())
 		
 		update_animation_params(target_pos - current_pos)
 		
-		self.global_position = current_pos.move_toward(target_pos, speed)
+		self.global_position = current_pos.move_toward(target_pos, speed * speed_modifier)
 		if self.global_position == target_pos:
 			current_path.pop_front()
+
+func get_terrain_speed_modifier(_tile_data: TileData) -> float:
+	if _tile_data:
+		if _tile_data.get_custom_data("Forest"):
+			return 0.8
+		elif _tile_data.get_custom_data("Hills"):
+			return 0.75
+		elif _tile_data.get_custom_data("Mountains"):
+			return 0.5
+	return 1.0
--- levels/map.gd
+++ levels/map.gd
@@ -5,10 +5,18 @@ extends TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	astar.region = self.get_used_rect()
+	var region: Rect2i = self.get_used_rect()
+	astar.region = region
 	astar.cell_size = Vector2(self.tile_set.tile_size)
 	astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
 	astar.update()
+	for x in region.size.x:
+		for y in region.size.y:
+			var tile_position: Vector2i = Vector2i(x + region.position.x,y + region.position.y)
+			var tile_data: TileData = self.get_cell_tile_data(0, tile_position)
+			if not tile_data or tile_data.get_custom_data("Impassible"):
+				astar.set_point_solid(tile_position)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
